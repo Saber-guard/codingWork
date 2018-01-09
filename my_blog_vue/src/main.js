7bd1aa749eb99405fcf4efbe10923c92a7be8479
saber-guard <zhaozhihao1100@163.com>
2018-01-09 18:11:12 +0800

    集成jquery

--- my_blog_vue/src/main.js
+++ my_blog_vue/src/main.js
@@ -2,10 +2,15 @@
 import Vue from '@/libs/Vue.js'
 import Router from '@/libs/Route.js'
 import Store from '@/libs/Store.js'
+import $ from 'jquery'
 
 //导入组件
 import App from '@/components/App'
 
+//导入全局样式
+import 'element-ui/lib/theme-chalk/index.css'
+
+var aaaaa = 123
 //调试模式
 Vue.config.productionTip = true
 
