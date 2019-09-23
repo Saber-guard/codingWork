5932cbd28fa9921922b2f807c65ee19415e4fd28
saber-guard <zhaozhihao1100@163.com>
2019-09-23 13:34:26 +0800

    支持多域名跨域

--- my_blog_php/app/Http/Middleware/CrossRequestMiddleware.php
+++ my_blog_php/app/Http/Middleware/CrossRequestMiddleware.php
@@ -9,12 +9,25 @@ class CrossRequestMiddleware
 {
 public function handle($request, Closure $next)
 	{
+		$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+
+		$allow_origin = array(
+		    'http://localhost:8080',
+		    'http://codingwork.com:8080',
+		    'http://codingwork.com',
+		    'http://codingwork.cn',
+		    'http://yunyun.codingwork.cn',
+		    'http://yunyun.codingwork.com',
+		);
 
 		$response = $next($request);
-		    $response->header('Access-Control-Allow-Origin', env('APP_ALLOW','http://localhost:8080'));
+		if (in_array($origin,$allow_origin)) {
+		    $response->header('Access-Control-Allow-Origin', $origin);
 		    $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
 		    $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
 		    $response->header('Access-Control-Allow-Credentials', 'true');
-		    return $response;
+		}
+
+	    return $response;
 	}
 }
