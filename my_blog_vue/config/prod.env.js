175ebb84cb991080f478c5a4dc03d1d95079b4cb
keallzhao <kaellzhao@tencent.com>
2020-05-20 18:00:48 +0800

    docker化

--- my_blog_vue/config/prod.env.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  API_URL: '"http://local.api.codingwork.cn"',
-  MQ_URL: '"mqtt://local.mq.codingwork.cn:1883"',
-  DE_BUG: 'false',
-}
